f74b25b4d4859a3a9213b54f808132d5b7bbb061
Aravindhan V <aravindhan.v@perfios.com>
2021-09-02 17:02:38 +0530

    ADDED: BubbleSort program added

---
+++ src/com/bridgelabz/algorithms/BubbleSort.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.algorithms;
+
+public class BubbleSort {
+	static void bubbleSort(int array[]) {
+		int length = array.length;
+		for (int i = 0; i < length - 1; i++) {
+			for (int j = 0; j < length - i - 1; j++) {
+				if (array[j] > array[j + 1]) {
+
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
+		printArray(array);
+	}
+
+	static void printArray(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+	}
+
+	public static void main(String[] args) {
+		int[] array = { 18, 86, 61, 31, 41, 5, 3 };
+		bubbleSort(array);
+	}
+}
