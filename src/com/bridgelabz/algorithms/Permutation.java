8c9decead4defe42f75ca907ddd5cde2e62e0011
Aravindhan V <aravindhan.v@perfios.com>
2021-09-02 16:00:14 +0530

    ADDED: Recursive string permutation added

---
+++ src/com/bridgelabz/algorithms/Permutation.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.algorithms;
+
+import java.util.*;
+
+public class Permutation {
+	private static void swap(char[] letters, int first, int second) {
+		char temp = letters[first];
+		letters[first] = letters[second];
+		letters[second] = temp;
+	}
+
+	private static void findPermutationsRecursive(char[] chars, int currentIndex) {
+		if (currentIndex == chars.length - 1) {
+			System.out.println(String.valueOf(chars));
+		}
+
+		for (int i = currentIndex; i < chars.length; i++) {
+			swap(chars, currentIndex, i);
+			findPermutationsRecursive(chars, currentIndex + 1);
+			swap(chars, currentIndex, i);
+		}
+	}
+
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter String:");
+		String word = scanner.nextLine();
+		char[] letters = word.toCharArray();
+		scanner.close();
+		
+		findPermutationsRecursive(letters,0);
+	}
+}
