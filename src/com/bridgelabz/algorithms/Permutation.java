9e498e83cdd4718808a1784a116011c5ef4292cc
Aravindhan V <aravindhan.v@perfios.com>
2021-09-02 21:51:02 +0530

    ADDED: findPermutationsIterative() to Permutation class

--- src/com/bridgelabz/algorithms/Permutation.java
+++ src/com/bridgelabz/algorithms/Permutation.java
@@ -9,18 +9,37 @@ public class Permutation {
 		letters[second] = temp;
 	}
 
-	private static void findPermutations(char[] chars, int currentIndex) {
+	private static void findPermutationsRecursive(char[] chars, int currentIndex) {
 		if (currentIndex == chars.length - 1) {
 			System.out.println(String.valueOf(chars));
 		}
 
 		for (int i = currentIndex; i < chars.length; i++) {
 			swap(chars, currentIndex, i);
-			findPermutations(chars, currentIndex + 1);
+			findPermutationsRecursive(chars, currentIndex + 1);
 			swap(chars, currentIndex, i);
 		}
 	}
 
+	private static void findPermutationsIterative(char[] chars, int currentIndex) {
+		String word = new String(chars);
+		if (word == null || word.length() == 0) {
+			return;
+		}
+		List<String> permuteList = new ArrayList<String>();
+		permuteList.add(String.valueOf(word.charAt(0)));
+		for (int index1 = 1; index1 < word.length(); index1++) {
+			for (int index2 = permuteList.size() - 1; index2 >= 0; index2--) {
+				String currentString = permuteList.remove(index2);
+				for (int index3 = 0; index3 <= currentString.length(); index3++) {
+					permuteList.add(
+							currentString.substring(0, index3) + word.charAt(index1) + currentString.substring(index3));
+				}
+
+			}
+		}
+		System.out.println(permuteList);
+	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -28,7 +47,8 @@ public class Permutation {
 		String word = scanner.nextLine();
 		char[] letters = word.toCharArray();
 		scanner.close();
-		
-		findPermutations(letters,0);
+
+		findPermutationsRecursive(letters, 0);
+		findPermutationsIterative(letters, 0);
 	}
 }
