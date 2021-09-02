c33c793032669d520efcfbebed7abef347c95025
Aravindhan V <aravindhan.v@perfios.com>
2021-09-02 21:56:26 +0530

    ADDED: GuessNumber program added

---
+++ src/com/bridgelabz/algorithms/GuessNumber.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.algorithms;
+
+import java.util.*;
+
+public class GuessNumber {
+	public static Scanner scanner = new Scanner(System.in);
+	public static double log2(int N) {
+		double result = (Math.log(N) / Math.log(2));
+        return result;
+	}
+	
+	public static void main(String[] args) {
+		int N = Integer.parseInt(args[0]);
+		if(log2(N) - Math.floor(log2(N))!=0) {
+			System.err.println("N must be a power of 2");
+			System.exit(0);
+		}
+		System.out.println("Think of a number between 0 and "+(N-1));
+		int number = guessNumber(0,N-1);
+		System.out.println("Enter 1 for YES and 0 for NO");
+	}
+	
+	public static int guessNumber(int start, int end) {
+		if(start==end) {
+			return start;
+		}
+		System.out.println("Is your number between "+(start)+" and "+((start+end)/2));
+		int response = scanner.nextInt();
+		int mid = (start+end)/2;
+		switch(response) {
+		case 1: return guessNumber(start,mid);
+		case 0: return guessNumber(mid+1,end);
+		default: System.err.println("Invalid Response");
+		System.exit(0);
+		return -1;
+		}
+	}
+}
