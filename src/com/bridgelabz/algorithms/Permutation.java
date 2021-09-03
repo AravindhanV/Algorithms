332e3767d06181a9f6b8d7f30d4b6867072b1565
Aravindhan V <aravindhan.v@perfios.com>
2021-09-03 06:21:38 +0530

    ADDED: iterative and recursive array comparison added

--- src/com/bridgelabz/algorithms/Permutation.java
+++ src/com/bridgelabz/algorithms/Permutation.java
@@ -3,6 +3,8 @@ package com.bridgelabz.algorithms;
 import java.util.*;
 
 public class Permutation {
+	private static List<String> permuteListRecursive = new ArrayList<>();
+	private static List<String> permuteListIterative = new ArrayList<>();
 	private static void swap(char[] letters, int first, int second) {
 		char temp = letters[first];
 		letters[first] = letters[second];
@@ -11,7 +13,7 @@ public class Permutation {
 
 	private static void findPermutationsRecursive(char[] chars, int currentIndex) {
 		if (currentIndex == chars.length - 1) {
-			System.out.println(String.valueOf(chars));
+			permuteListRecursive.add(String.valueOf(chars));
 		}
 
 		for (int i = currentIndex; i < chars.length; i++) {
@@ -24,21 +26,20 @@ public class Permutation {
 	private static void findPermutationsIterative(char[] chars, int currentIndex) {
 		String word = new String(chars);
 		if (word == null || word.length() == 0) {
-			return;
+			return ;
 		}
-		List<String> permuteList = new ArrayList<String>();
-		permuteList.add(String.valueOf(word.charAt(0)));
+		
+		permuteListIterative.add(String.valueOf(word.charAt(0)));
 		for (int index1 = 1; index1 < word.length(); index1++) {
-			for (int index2 = permuteList.size() - 1; index2 >= 0; index2--) {
-				String currentString = permuteList.remove(index2);
+			for (int index2 = permuteListIterative.size() - 1; index2 >= 0; index2--) {
+				String currentString = permuteListIterative.remove(index2);
 				for (int index3 = 0; index3 <= currentString.length(); index3++) {
-					permuteList.add(
+					permuteListIterative.add(
 							currentString.substring(0, index3) + word.charAt(index1) + currentString.substring(index3));
 				}
 
 			}
 		}
-		System.out.println(permuteList);
 	}
 
 	public static void main(String[] args) {
@@ -50,5 +51,21 @@ public class Permutation {
 
 		findPermutationsRecursive(letters, 0);
 		findPermutationsIterative(letters, 0);
+		
+		System.out.println(permuteListIterative);
+		System.out.println(permuteListRecursive);
+		Collections.sort(permuteListIterative);
+		Collections.sort(permuteListRecursive);
+		if(permuteListIterative.size() == permuteListRecursive.size()) {
+			for(int index=0;index<permuteListIterative.size();index++) {
+				if(!permuteListIterative.get(index).equals(permuteListRecursive.get(index))) {
+					System.out.println("Arrays Don't Match");
+					return;
+				}
+			}
+			System.out.println("Arrays Match");
+		} else {
+			System.out.println("Arrays don't match");
+		}
 	}
 }
